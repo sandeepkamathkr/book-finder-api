25ff1848ad7b5c9a603feca072ec1559d13b6b6f
Sandeep <sandeepkamathkr@gmail.com>
2020-06-18 18:26:59 +1000

    intial commit with spring boot configuration from spring initializr https://start.spring.io/

---
+++ src/main/java/com/sandeepkamathkr/webservice/bookfinderapi/BookFinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.sandeepkamathkr.webservice.bookfinderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFinderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookFinderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandeepkamathkr/webservice/bookfinderapi/BookFinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeepkamathkr.webservice.bookfinderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookFinderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
